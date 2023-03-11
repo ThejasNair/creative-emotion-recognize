53c350720736b3615030437626a5c1fd7609b7f5
ThejasNair <96673994+ThejasNair@users.noreply.github.com>
2023-03-11 18:04:08 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+document.getElementById("anger1").style.visibility = "hidden";
+document.getElementById("anger2").style.visibility = "hidden";
+document.getElementById("anger3").style.visibility = "hidden";
+document.getElementById("anger4").style.visibility = "hidden";
+document.getElementById("anger5").style.visibility = "hidden";
+document.getElementById("anger6").style.visibility = "hidden";
+
+function angerbegins() {
+    document.getElementById("whydoievenhavetogivethisanid").style.backgroundColor = "red";
+    document.getElementById("whydoievenhavetogivethisanid").style.color = "black";
+    document.getElementById("IMSOANGRY").innerHTML = "WISH GRANTED";
+    document.getElementById("emoji1").style.visibility = "hidden";
+    document.getElementById("emoji2").style.visibility = "hidden";
+    document.getElementById("emoji3").style.visibility = "hidden";
+    document.getElementById("emoji4").style.visibility = "hidden";
+    document.getElementById("emoji5").style.visibility = "hidden";
+    document.getElementById("emoji6").style.visibility = "hidden";
+    document.getElementById("anger1").style.visibility = "visible";
+document.getElementById("anger2").style.visibility = "visible";
+document.getElementById("anger3").style.visibility = "visible";
+document.getElementById("anger4").style.visibility = "visible";
+document.getElementById("anger5").style.visibility = "visible";
+document.getElementById("anger6").style.visibility = "visible";
+
+document.getElementById("happy").innerHTML = "Are you Angry?";
+document.getElementById("angry").innerHTML = "Are you Angry?";
+}
+
+function confused() {
+    document.getElementById("confused1").innerHTML = "In order for it to recognize your face you must make your face look like this";
+    document.getElementById("confused2").src = "https://tse2.mm.bing.net/th?id=OIP.Ck2Usj84bIh_SiI2QJ7b-QHaEp&pid=Api&P=0";
+    document.getElementById("confused3").src = "https://tse3.mm.bing.net/th?id=OIP.kg-byvJhpT1fyrzvzkNy0gHaE5&pid=Api&P=0";
+    document.getElementById("confused4").src = "https://tse4.explicit.bing.net/th?id=OIP.pj0A8F4L5dYBhNKGoB3yUQHaFJ&pid=Api&P=0";
+}
